79515a2608c371576d046b80a6c6f15e9c3f27b6
LucasJurgec <105923018@student.swin.edu.au>
2025-05-08 16:46:48 +1000

    html tags

--- process.php
+++ process.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Metadata and page setup -->
+	<meta charset="UTF-8">
+	<meta name="description" content="Home page">
+	<meta name="keywords" content="HTML5, tags">
+	<meta name="author" content="Lucas Jurgec">
+	<title>Home Page</title>
+
+</head>
+<body>
 <?php
     session_start();
     $username = $_POST['username'];
@@ -10,3 +22,5 @@
         echo "Invalid login. <a href='login.html'>Try again</a>";
     }
 ?>
+</body>
+</html>
--- welcome.php
+++ welcome.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Metadata and page setup -->
+	<meta charset="UTF-8">
+	<meta name="description" content="Home page">
+	<meta name="keywords" content="HTML5, tags">
+	<meta name="author" content="Lucas Jurgec">
+	<title>Home Page</title>
+
+</head>
+<body>
 <?php
     session_start();
     if (isset($_SESSION['user'])){
@@ -6,3 +18,5 @@
         header('Location:login.html')
     }
 ?>
+</body>
+</html>
