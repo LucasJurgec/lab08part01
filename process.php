ea9178cfea484c0642215497f94b402fc4aec4c3
LucasJurgec <105923018@student.swin.edu.au>
2025-05-09 10:33:03 +1000

    h

--- process.php
+++ process.php
@@ -10,17 +10,17 @@
 
 </head>
 <body>
-<?php
-    session_start();
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+    <?php
+        session_start();
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-    if ($username == 'admin' && $password == 'password123'){
-        $_SESSION['user'] = $username; header('Location:welcome.php');
-    }
-    else {
-        echo "Invalid login. <a href='login.html'>Try again</a>";
-    }
-?>
+        if ($username == 'admin' && $password == 'password123'){
+            $_SESSION['user'] = $username; header('Location:welcome.php');
+        }
+        else {
+            echo "Invalid login. <a href='login.html'>Try again</a>";
+        }
+    ?>
 </body>
 </html>
